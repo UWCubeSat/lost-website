9fcc3b12d373127cc56886191fbeabbb44bc51cc
Daniel Zhang <72416560+eminentquasar34@users.noreply.github.com>
2025-05-28 22:11:34 -0700

    Fixed submit button

--- src/router.tsx
+++ src/router.tsx
@@ -3,26 +3,22 @@ import { createBrowserRouter } from 'react-router'
 import Index from './pages/Index'
 import Notfound from './pages/Notfound'
 import { Analyze } from './pages/Analyze/analyze'
+import { Generate } from './pages/Generate/generate'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: (
-        <Index />
-    ),
+    element: <Index />,
   },
   {
     path: 'analyze',
-    element: (
-        <Analyze />
-    ),
+    element: <Analyze />,
   },
   {
     path: '*',
-    element: (
-        <Notfound />
-    ),
+    element: <Notfound />,
   },
+  { path: 'generate', element: <Generate /> },
 ])
 
 export default router
